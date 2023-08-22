ca159becb15c76aa974bc8fae238ac4952cb5d81
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-08-22 16:43:47 +0100

    implemented profile tabs

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,11 @@
 import ProfileHeader from '@/components/shared/ProfileHeader'
+import { TabsTrigger, Tabs, TabsList, TabsContent } from '@/components/ui/tabs'
 import { fetchUser } from '@/lib/actions/user.actions'
 import { currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
+import Image from 'next/image'
+import { profileTabs } from '@/constants'
+import PostsTab from '@/components/shared/PostsTab'
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser()
@@ -22,6 +26,44 @@ const Page = async ({ params }: { params: { id: string } }) => {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
+
+      <div className='mt-9'>
+        <Tabs defaultValue='posts' className='w-full'>
+          <TabsList className='tab'>
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className='object-contain'
+                />
+                <p className='max-sm:hidden'>{tab.label}</p>
+                {tab.label === 'Posts' && (
+                  <p className='ml-1 rounded-full bg-light-4 px-2 py-1 text-tiny-medium text-light-2'>
+                    {userInfo?.posts?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className='w-full text-light-1'
+            >
+              <PostsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType='User'
+              />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </section>
   )
 }
---
+++ components/shared/PostsTab.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  currentUserId: string
+  accountId: string
+  accountType: string
+}
+
+const PostsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  //TODO: Fetch profile posts
+  return <section>Threads Tab</section>
+}
+
+export default PostsTab
