62a2accbc35880e06518a94c5f90a040e327e1d6
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-08-22 16:58:47 +0100

    fetch user posts

--- components/shared/PostsTab.tsx
+++ components/shared/PostsTab.tsx
@@ -1,3 +1,7 @@
+import { fetchUserPosts } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
+import PostCard from '../cards/PostCard'
+
 interface Props {
   currentUserId: string
   accountId: string
@@ -5,8 +9,40 @@ interface Props {
 }
 
 const PostsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-  //TODO: Fetch profile posts
-  return <section>Threads Tab</section>
+  let result = await fetchUserPosts(accountId)
+
+  if (!result) redirect('/')
+  return (
+    <section className='mt-9 flex flex-col gap-10'>
+      {result.posts.map((post: any) => (
+        <PostCard
+          key={post._id}
+          id={post._id}
+          currentUserId={currentUserId}
+          parentId={post.parentId}
+          content={post.text}
+          author={
+            accountType === 'User'
+              ? {
+                  name: result.name,
+                  username: result.username,
+                  image: result.image,
+                  id: result.id,
+                }
+              : {
+                  name: post.author.name,
+                  username: post.author.username,
+                  image: post.author.image,
+                  id: post.author.id,
+                }
+          } //todo
+          community={post.community} //todo
+          createdAt={post.createdAt}
+          comments={post.children}
+        />
+      ))}
+    </section>
+  )
 }
 
 export default PostsTab
